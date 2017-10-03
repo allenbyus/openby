f42a42673e02ea3d68f11c63a808c20a853f1dd8
allenbybid <28820818+allenbybid@users.noreply.github.com>
2017-10-04 07:14:27 +0800

    Update in.php
    增加头部和底部css&js入口

--- include/in.php
+++ include/in.php
@@ -6,9 +6,9 @@ echo <<<HTML
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>{$page['title']}-{$main['sitename']}</title>
-
     <!-- MZUI CSS file -->
-    <link href="dist/css/mzui.min.css" rel="stylesheet">
+    <link href="/dist/css/mzui.min.css" rel="stylesheet">
+{$page['tool']}
   </head>
   <body>
 HTML;
@@ -16,7 +16,10 @@ include("./{$tfloder}/{$ac}/tindex.php");
 echo <<<HTML
 <footer> <p><font size="1px"><center>{$page['footer']}<br>Copyright (c) 2017<strong>{$main['sitename']}</strong> All Rights Reserved.</center></font></p><p><center><infos></infos></center></p></footer>
     <!-- MZUI JS file -->
-    <script src="dist/js/mzui.min.js"></script>
+<script src="/dist/js/jquery.min.js"></script>
+    <script src="/dist/js/mzui.min.js">
+</script>
+{$page['fool']}
   </body>
 </html>
 HTML;
