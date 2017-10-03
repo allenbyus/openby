f6096b88a32979be0ba19ee9c9d2c21d5926dc50
allenbybid <28820818+allenbybid@users.noreply.github.com>
2017-10-04 07:47:43 +0800

    Update xhtml.php

--- fileadmin/xhtml.php
+++ fileadmin/xhtml.php
@@ -1,9 +1,8 @@
 <?php
-function xhtml_head()
-{
-    $name = NAME;
-    $title = func_get_arg(0);
-    echo <<<XHTML
+function xhtml_head() {
+$name=NAME;
+$title=func_get_arg(0);
+echo <<<XHTML
 <!DOCTYPE html PUBLIC "-//WAPFORUM//DTD XHTML Mobile 1.0//EN" "http://www.wapforum.org/DTD/xhtml-mobile10.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
     <head>
@@ -14,18 +13,18 @@ function xhtml_head()
         <div id="header">
 	    我一直在开辟属于我的天空
         </div>
+
 XHTML;
 }
-
-function xhtml_footer()
-{
-    echo <<<XHTML
+function xhtml_footer() {
+$time=time();
+echo <<<XHTML
         <div id="footer">
-	    By：<a href="mailto:xiaoqidun@gmail.com">Xiaoqidun@Gmail.Com</a>
+	  {$_SERVER['HTTP_USER_AGENT']}<br>{$time}
         </div>
     </body>
 </html>
+
 XHTML;
 }
-
 ?>
