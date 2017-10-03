899f839a59b5d8e6de1513d8ab93f640e6c1abfd
allenbybid <28820818+allenbybid@users.noreply.github.com>
2017-10-04 06:09:04 +0800

    Add files via upload

---
+++ include/config.php
@@ -0,0 +1,3 @@
+<?php
+$main['sitename']="ALLENBY";
+?>
---
+++ include/in.php
@@ -0,0 +1,24 @@
+<?php
+echo <<<HTML
+<!DOCTYPE html>
+<html lang="zh-cn">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>{$page['title']}-{$main['sitename']}</title>
+
+    <!-- MZUI CSS file -->
+    <link href="dist/css/mzui.min.css" rel="stylesheet">
+  </head>
+  <body>
+HTML;
+include("./{$tfloder}/{$ac}/tindex.php");
+echo <<<HTML
+<footer> <p><font size="1px"><center>{$page['footer']}<br>Copyright (c) 2017<strong>{$main['sitename']}</strong> All Rights Reserved.</center></font></p><p><center><infos></infos></center></p></footer>
+    <!-- MZUI JS file -->
+    <script src="dist/js/mzui.min.js"></script>
+  </body>
+</html>
+HTML;
+
+?>
