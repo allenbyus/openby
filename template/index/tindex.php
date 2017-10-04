eb222565980de0a5867ba5b7cb9e3a7aed9a91eb
allenbybid <28820818+allenbybid@users.noreply.github.com>
2017-10-04 08:27:21 +0800

    Update tindex.php

--- template/index/tindex.php
+++ template/index/tindex.php
@@ -1,5 +1,8 @@
 <?php
 echo <<<HTML
+<video class="bg-video" autoplay loop muted webkit-playsinline poster="/uploads/1506312472.jpg">
+        <source src="/uploads/1507076757.mp4" type="video/mp4">
+    </video>
 <div class="wrap">
 <div class="cmusic fltL" id="cm1"></div>
 </div>
