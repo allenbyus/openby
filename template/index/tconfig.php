8717510ad854d627acc435b8363cc184c285fd4b
allenbybid <28820818+allenbybid@users.noreply.github.com>
2017-10-04 12:37:56 +0800

    Update tconfig.php

--- template/index/tconfig.php
+++ template/index/tconfig.php
@@ -5,6 +5,8 @@ $page['fool']=<<<HTML
 <script type="text/javascript" src="/dist/js/demo.js"></script>
 HTML;
 $page['tool']=<<<HTML
+<link rel="stylesheet" href="/dist/css/offline-theme-slide-indicator.css">
+<link rel="stylesheet" href="/dist/css/offline-language-english.css">
 <link href="/dist/css/main.css" rel="stylesheet">
 HTML;
 ?>
