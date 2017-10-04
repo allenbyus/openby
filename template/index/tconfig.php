a50e26c0e6be3418ab9177ccff55c7c413b8ff17
allenbybid <28820818+allenbybid@users.noreply.github.com>
2017-10-04 08:18:04 +0800

    Update tconfig.php

--- template/index/tconfig.php
+++ template/index/tconfig.php
@@ -1,3 +1,10 @@
 <?php
 $page['title']="主页";
+$page['fool']=<<<HTML
+<script type="text/javascript" src="/dist/js/cPlayer.min.js" id="cmScript"></script>
+<script type="text/javascript" src="/dist/js/demo.js"></script>
+HTML;
+$page['tool']=<<<HTML
+<link href="/dist/css/main.css" rel="stylesheet">
+HTML;
 ?>
