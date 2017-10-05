07ae84f4b3043eafc685d8e47fa12116e1a648b1
Taylor Allen <28820818+allenbybid@users.noreply.github.com>
2017-10-05 22:06:40 +0800

    Create tconfig.php

---
+++ template/404/tconfig.php
@@ -0,0 +1,10 @@
+<?php
+$page['title']="页面没有找到";
+$page['fool']=<<<HTML
+<script type="text/javascript" src="/dist/js/offline.min.js"></script>
+HTML;
+$page['tool']=<<<HTML
+<link rel="stylesheet" href="/dist/css/offline-theme-slide-indicator.css">
+<link rel="stylesheet" href="/dist/css/offline-language-english.css">
+HTML;
+?>
