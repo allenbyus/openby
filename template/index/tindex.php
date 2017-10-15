e62ecfefd0eaa95da43bf7e0a7685e64eb698366
Taylor Allen <28820818+allenbybid@users.noreply.github.com>
2017-10-15 11:47:32 +0800

    Delete tindex.php

--- template/index/tindex.php
+++
@@ -1,8 +0,0 @@
-<?php
-echo <<<HTML
-<video class="bg-video" autoplay loop muted webkit-playsinline poster="/uploads/1506312472.jpg">
-        <source src="/uploads/1507076757.mp4" type="video/mp4">
-    </video>
-<div class="cmusic fltL" id="cm1"></div>
-HTML;
-?>
