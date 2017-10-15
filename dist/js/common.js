59bce1207ed4e852b1a528f8b18862764f7b909c
Taylor Allen <28820818+allenbybid@users.noreply.github.com>
2017-10-15 14:42:51 +0800

    Update common.js

--- dist/js/common.js
+++ dist/js/common.js
@@ -1,6 +1,10 @@
+function randomHexColor(){
+//随机生成十六进制颜色
+return '#' + ('00000' + (Math.random() * 0x1000000 << 0).toString(16)).substr(-6);
+}
 $("#fakeLoader").fakeLoader({
 timeToHide:180000,
-bgColor:"#2ecc71",
+bgColor:randomHexColor(),
 spinner:"spinner1"
 });
 if(window.top==window.self){
@@ -10,6 +14,6 @@ document.body.style.backgroundImage="url('/uploads/1507074542.jpg')";
 function kloader(){
 $("#fakeLoader").hide();
 }
-$(document).ready(function(){
-kloader();
-});
+window.onload=function(){
+setTimeout(kloader(),1500);
+};
