c0d374ecd8a5a5759b2b2fb9a4d753bd20e440e2
Taylor Allen <28820818+allenbybid@users.noreply.github.com>
2017-10-15 14:02:04 +0800

    Update in.php

--- include/in.php
+++ include/in.php
@@ -1,7 +1,7 @@
 <?php
 echo <<<HTML
 <!DOCTYPE html>
-<!--html manifest="cache.php"-->
+<html manifest="cache.php">
 <html lang="zh-cn">
   <head>
     <meta charset="utf-8">
@@ -9,10 +9,12 @@ echo <<<HTML
     <title>{$page['title']}-{$main['sitename']}</title>
     <!-- MZUI CSS file -->
     <link href="/dist/css/mzui.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="/dist/css/fakeLoader.css">
     <link href="/dist/css/common.css" rel="stylesheet">
 {$page['tool']}
   </head>
   <body>
+<div id="fakeLoader"></div>
   <div class="contents">
 HTML;
 include("./{$tfloder}/{$ac}/tindex.php");
@@ -24,6 +26,7 @@ echo <<<HTML
     <!-- MZUI JS file -->
 <script src="/dist/js/jquery.min.js"></script>
 <script src="/dist/js/mzui.min.js"></script>
+<script src="/dist/js/fakeLoader.min.js"></script>
 <script src="/dist/js/common.js"></script>
 <script src="/dist/js/cache.js"></script>
 {$page['fool']}
