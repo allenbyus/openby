67b88e3de12dd8727638a45e1c8b6ca0a5b68532
Taylor Allen <28820818+allenbybid@users.noreply.github.com>
2017-10-15 15:10:10 +0800

    Update common.js

--- dist/js/common.js
+++ dist/js/common.js
@@ -14,6 +14,23 @@ document.body.style.backgroundImage="url('/uploads/1507074542.jpg')";
 function kloader(){
 $("#fakeLoader").hide();
 }
-window.onload=function(){
-setTimeout(kloader(),1500);
-};
+function addLoadListener(fn){
+    if (typeof window.addEventListener != 'undefined'){
+        window.addEventListener('load',fn,false);
+    }else if(typeof document.addEventListener != 'undefined'){
+        document.addEventListener('load',fn,false);
+    }else if (typeof window.attachEvent != 'undefined'){
+        window.attachEvent('onload',fn);
+    }else{
+        var oldfn = window.onload;
+        if(typeof window.onload != 'function'){
+            window.onload = fn;
+        }else{
+            window.onload = function(){
+                oldfn();
+                fn();
+            };
+        }
+    }
+}
+addLoadListener(kloader);
