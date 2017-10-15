fe647169e458bdd393a78f9ac565c3935c84deef
Taylor Allen <28820818+allenbybid@users.noreply.github.com>
2017-10-15 14:28:32 +0800

    Update common.js

--- dist/js/common.js
+++ dist/js/common.js
@@ -1,9 +1,15 @@
-$("#fakeloader").fakeLoader({
-                    timeToHide:1200,
-                    bgColor:"#2ecc71",
-                    spinner:"spinner1"
-                });
+$("#fakeLoader").fakeLoader({
+timeToHide:180000,
+bgColor:"#2ecc71",
+spinner:"spinner1"
+});
 if(window.top==window.self){
 document.body.style.backgroundColor="#FFCC80";
 document.body.style.backgroundImage="url('/uploads/1507074542.jpg')";
 }
+function kloader(){
+$("#fakeLoader").hide();
+}
+$(document).ready(function(){
+kloader();
+});
