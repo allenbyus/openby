962b12483970b3726a53740b58e0c33ecafb393c
Taylor Allen <28820818+allenbybid@users.noreply.github.com>
2017-10-15 15:03:17 +0800

    Update and rename template/clock/tconfig.php to template/canvas/tconfig.php

--- template/canvas/tconfig.php
+++ template/canvas/tconfig.php
@@ -1,13 +1,14 @@
 <?php
-$page['title']="时钟";
+$page['title']="画布";
 $page['fool']=<<<HTML
 <script type="text/javascript" src="/dist/js/offline.min.js"></script>
-<script type="text/javascript" src="/dist/js/vue.min.js"></script>
-<script type="text/javascript" src="/dist/js/clock.js"></script>
+<script type="text/javascript" src="/dist/js/canvas.js"></script>
 HTML;
 $page['tool']=<<<HTML
 <link rel="stylesheet" href="/dist/css/offline-theme-slide-indicator.css">
 <link rel="stylesheet" href="/dist/css/offline-language-english.css">
 <link href="/dist/css/main.css" rel="stylesheet">
+<link href="/dist/css/canvas.css" rel="stylesheet">
 HTML;
 ?>
+
