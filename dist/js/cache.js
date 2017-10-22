4f25beaec666cd615650256c54fb0225b43c447d
Taylor Allen <28820818+allenbybid@users.noreply.github.com>
2017-10-22 18:56:10 +0800

    Delete cache.js

--- dist/js/cache.js
+++
@@ -1,6 +0,0 @@
-applicationCache.addEventListener('updateready', function(e) {
-        if (applicationCache.status == applicationCache.UPDATEREADY) {
-            applicationCache.swapCache(); //使用新版本资源
-            window.location.reload(); //刷新页面
-        }
-    }, false);
