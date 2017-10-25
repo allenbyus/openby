de26a7ed4bc7b108fc4ff4dc8db6386d17744a6a
Taylor Allen <28820818+allenbybid@users.noreply.github.com>
2017-10-25 21:51:59 +0800

    Update demo.js
    改为外链形式。

--- dist/js/demo.js
+++ dist/js/demo.js
@@ -1,38 +1,57 @@
+function Path(){
+    const script=document.getElementById('cmScript')
+    let uri=script.attributes.src.nodeValue.split('/'),
+    path = ''
+    for(let i = 0; i < uri.length - 1; i++) {
+      path += uri[i] + '/'
+    }
+    return path
+  }
+//dist/js/k.js
+function uprefiexes(){
+var js = document.scripts;
+var url =js[js.length - 1].src;
+url=url.substring(0,url.indexOf('/',8));
+//url=url.substring(0, url.lastIndexOf('/'));
+return url;
+}
+//http://xxx.com/
+var upre=uprefiexes();
 var playlist=[
   {
     'title': 'Another World',
     'artist': 'A応P',
-    'cover': '/uploads/1506255575.jpg',
-    'file': '/uploads/1506746043.mp3',
-    'lrc': '/uploads/1506252456.lrc'
+    'cover': upre+'/uploads/1506255575.jpg',
+    'file': upre+'/uploads/1506746043.mp3',
+    'lrc': upre+'/uploads/1506252456.lrc'
   },
 {
     'title': 'only my railgun',
     'artist': 'fripSide',
-    'cover': '/uploads/1506256002.jpg',
-    'file': '/uploads/1506746017.mp3',
-    'lrc': '/uploads/1506745992.lrc'
+    'cover': upre+'/uploads/1506256002.jpg',
+    'file': upre+'/uploads/1506746017.mp3',
+    'lrc': upre+'/uploads/1506745992.lrc'
   },
 {
     'title': 'メグメル',
     'artist': 'riya',
-    'cover': '/uploads/1506746393.jpg',
-    'file': '/uploads/1506746461.mp3',
-    'lrc': '/uploads/1506745863.lrc'
+    'cover': upre+'/uploads/1506746393.jpg',
+    'file': upre+'/uploads/1506746461.mp3',
+    'lrc': upre+'/uploads/1506745863.lrc'
   },
 {
     'title': '影二つ',
     'artist': 'Riya(日中)',
-    'cover': '/uploads/1506746191.jpg',
-    'file': '/uploads/1506746773.mp3',
-    'lrc': '/uploads/1506747009.lrc'
+    'cover': upre+'/uploads/1506746191.jpg',
+    'file': upre+'/uploads/1506746773.mp3',
+    'lrc': upre+'/uploads/1506747009.lrc'
   },
  {
     'title': 'Masquerade kiss',
     'artist': '田村ゆかり',
-    'cover': '/uploads/1508322205.jpg',
-    'file': '/uploads/1508321457.mp3',
-    'lrc': '/uploads/1508322416.lrc'
+    'cover': upre+'/uploads/1508322205.jpg',
+    'file': upre+'/uploads/1508321457.mp3',
+    'lrc': upre+'/uploads/1508322416.lrc'
   },
 ];
 var cm1 = new cPlayer({
