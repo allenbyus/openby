216c4d29eb6b553fb22c80854193bd81de8c2613
Taylor Allen <28820818+allenbybid@users.noreply.github.com>
2017-11-09 06:30:43 +0800

    Update common.js

--- dist/js/common.js
+++ dist/js/common.js
@@ -1,7 +1,26 @@
+function Path(){
+    const script=document.getElementById('cmScript')
+    let uri=script.attributes.src.nodeValue.split('/'),
+    path = ''
+    for(let i = 0; i < uri.length - 1; i++) {
+      path += uri[i] + '/'
+    }
+    return path
+  }
+//dist/js/k.js
+function uprefiexes(){
+var js = document.scripts;
+var url =js[js.length - 1].src;
+url=url.substring(0,url.indexOf('/',8));
+//url=url.substring(0, url.lastIndexOf('/'));
+return url;
+}
+//http://xxx.com/
 function randomHexColor(){
 //随机生成十六进制颜色
 return '#' + ('00000' + (Math.random() * 0x1000000 << 0).toString(16)).substr(-6);
 }
+NProgress.start();
 $("#fakeLoader").fakeLoader({
 timeToHide:180000,
 bgColor:randomHexColor(),
@@ -9,10 +28,12 @@ spinner:"spinner1"
 });
 if(window.top==window.self){
 document.body.style.backgroundColor="#FFCC80";
-document.body.style.backgroundImage="url('/uploads/1507074542.jpg')";
+document.body.style.backgroundImage="url(uprefiexes+'/uploads/1507074542.jpg')";
 }
 function kloader(){
 $("#fakeLoader").hide();
+NProgress.done();
+NProgress.remove();
 }
 function addLoadListener(fn){
     if (typeof window.addEventListener != 'undefined'){
