d04304bf0a92f68e065926dcffdefb0c57c15dbe
Taylor Allen <28820818+allenbybid@users.noreply.github.com>
2018-09-01 10:08:49 +0800

    Update on 20180901
    1.增加手机访问判断函数
    2.增加获取IP函数,共2个

--- include/function.php
+++ include/function.php
@@ -1,4 +1,25 @@
 <?php
+function isMobile(){
+	// 如果有HTTP_X_WAP_PROFILE则一定是移动设备
+	if(isset($_SERVER['HTTP_X_WAP_PROFILE'])) return true;
+	// 如果via信息含有wap则一定是移动设备,部分服务商会屏蔽该信息
+	if(isset($_SERVER['HTTP_VIA']))return stristr($_SERVER['HTTP_VIA'], "wap") ? true : false;
+	// 野蛮方法，判断手机发送的客户端标志,兼容性有待提高
+	if(isset($_SERVER['HTTP_USER_AGENT'])){
+		$clientkeywords = array ('nokia','sony','ericsson','mot','samsung','htc','sgh','lg','sharp','sie-','philips','panasonic','alcatel','lenovo','iphone','ipod','blackberry','meizu','android','netfront','symbian','ucweb','windowsce','palm','operamini','operamobi','openwave','nexusone','cldc','midp','wap','mobile');
+		// 从HTTP_USER_AGENT中查找手机浏览器的关键字
+		if (preg_match("/(" . implode('|', $clientkeywords) . ")/i", strtolower($_SERVER['HTTP_USER_AGENT'])))
+		return true;
+	}
+	// 协议法，因为有可能不准确，放到最后判断
+	if(isset($_SERVER['HTTP_ACCEPT'])){
+		// 如果只支持wml并且不支持html那一定是移动设备
+        // 如果支持wml和html但是wml在html之前则是移动设备
+		if((strpos($_SERVER['HTTP_ACCEPT'], 'vnd.wap.wml') !== false) && (strpos($_SERVER['HTTP_ACCEPT'], 'text/html') === false || (strpos($_SERVER['HTTP_ACCEPT'], 'vnd.wap.wml') < strpos($_SERVER['HTTP_ACCEPT'], 'text/html'))))
+		return true;
+	}
+	return false;
+}
 function cpost($url,$data){
 	$ch=curl_init();
 	curl_setopt($ch,CURLOPT_URL,$url);
@@ -12,9 +33,24 @@ function cpost($url,$data){
 	curl_close($ch);
 	return $result;
 }
-
-
-
+function getRealIp()
+{
+  $ip=false;
+  if(!empty($_SERVER["HTTP_CLIENT_IP"])){
+    $ip = $_SERVER["HTTP_CLIENT_IP"];
+  }
+  if (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+    $ips = explode (", ", $_SERVER['HTTP_X_FORWARDED_FOR']);
+    if ($ip) { array_unshift($ips, $ip); $ip = FALSE; }
+    for ($i = 0; $i < count($ips); $i++) {
+      if (!eregi ("^(10│172.16│192.168).", $ips[$i])) {
+        $ip = $ips[$i];
+        break;
+      }
+    }
+  }
+  return ($ip ? $ip : $_SERVER['REMOTE_ADDR']);
+}
 function cget($url){
 	$ch=curl_init();
 	curl_setopt($ch,CURLOPT_URL,$url);
@@ -45,65 +81,65 @@ function get_ip(){
 	}
 	return $cip;
 }
-function authcode($string, $operation = 'D', $key = '', $expiry = 0) {  
-    // 动态密匙长度，相同的明文会生成不同密文就是依靠动态密匙  
-    $ckey_length = 4;  
-      
-    // 密匙  
-    $key = md5($key ? $key : $GLOBALS['discuz_auth_key']);  
-      
-    // 密匙a会参与加解密  
-    $keya = md5(substr($key, 0, 16));  
-    // 密匙b会用来做数据完整性验证  
-    $keyb = md5(substr($key, 16, 16));  
-    // 密匙c用于变化生成的密文  
+function authcode($string, $operation = 'D', $key = '', $expiry = 0) {
+    // 动态密匙长度，相同的明文会生成不同密文就是依靠动态密匙
+    $ckey_length = 4;
+
+    // 密匙
+    $key = md5($key ? $key : $GLOBALS['discuz_auth_key']);
+
+    // 密匙a会参与加解密
+    $keya = md5(substr($key, 0, 16));
+    // 密匙b会用来做数据完整性验证
+    $keyb = md5(substr($key, 16, 16));
+    // 密匙c用于变化生成的密文
     $keyc = $ckey_length ? ($operation == 'D' ? substr($string, 0, $ckey_length):
-substr(md5(microtime()), -$ckey_length)) : '';  
-    // 参与运算的密匙  
-    $cryptkey = $keya.md5($keya.$keyc);  
-    $key_length = strlen($cryptkey);  
+substr(md5(microtime()), -$ckey_length)) : '';
+    // 参与运算的密匙
+    $cryptkey = $keya.md5($keya.$keyc);
+    $key_length = strlen($cryptkey);
     // 明文，前10位用来保存时间戳，解密时验证数据有效性，10到26位用来保存$keyb(密匙b)，
-//解密时会通过这个密匙验证数据完整性  
-    // 如果是解码的话，会从第$ckey_length位开始，因为密文前$ckey_length位保存 动态密匙，以保证解密正确  
-    $string = $operation == 'D' ? base64_decode(substr($string, $ckey_length)) : 
-sprintf('%010d', $expiry ? $expiry + time() : 0).substr(md5($string.$keyb), 0, 16).$string;  
-    $string_length = strlen($string);  
-    $result = '';  
-    $box = range(0, 255);  
-    $rndkey = array();  
-    // 产生密匙簿  
-    for($i = 0; $i <= 255; $i++) {  
-        $rndkey[$i] = ord($cryptkey[$i % $key_length]);  
-    }  
-    // 用固定的算法，打乱密匙簿，增加随机性，好像很复杂，实际上对并不会增加密文的强度  
-    for($j = $i = 0; $i < 256; $i++) {  
-        $j = ($j + $box[$i] + $rndkey[$i]) % 256;  
-        $tmp = $box[$i];  
-        $box[$i] = $box[$j];  
-        $box[$j] = $tmp;  
-    }  
-    // 核心加解密部分  
-    for($a = $j = $i = 0; $i < $string_length; $i++) {  
-        $a = ($a + 1) % 256;  
-        $j = ($j + $box[$a]) % 256;  
-        $tmp = $box[$a];  
-        $box[$a] = $box[$j];  
-        $box[$j] = $tmp;  
-        // 从密匙簿得出密匙进行异或，再转成字符  
-        $result .= chr(ord($string[$i]) ^ ($box[($box[$a] + $box[$j]) % 256]));  
-    }  
-    if($operation == 'D') { 
-        // 验证数据有效性，请看未加密明文的格式  
-        if((substr($result, 0, 10) == 0 || substr($result, 0, 10) - time() > 0) && 
-substr($result, 10, 16) == substr(md5(substr($result, 26).$keyb), 0, 16)) {  
-            return substr($result, 26);  
-        } else {  
-            return '';  
-        }  
-    } else {  
-        // 把动态密匙保存在密文里，这也是为什么同样的明文，生产不同密文后能解密的原因  
-        // 因为加密后的密文可能是一些特殊字符，复制过程可能会丢失，所以用base64编码  
-        return $keyc.str_replace('=', '', base64_encode($result));  
-    }  
+//解密时会通过这个密匙验证数据完整性
+    // 如果是解码的话，会从第$ckey_length位开始，因为密文前$ckey_length位保存 动态密匙，以保证解密正确
+    $string = $operation == 'D' ? base64_decode(substr($string, $ckey_length)) :
+sprintf('%010d', $expiry ? $expiry + time() : 0).substr(md5($string.$keyb), 0, 16).$string;
+    $string_length = strlen($string);
+    $result = '';
+    $box = range(0, 255);
+    $rndkey = array();
+    // 产生密匙簿
+    for($i = 0; $i <= 255; $i++) {
+        $rndkey[$i] = ord($cryptkey[$i % $key_length]);
+    }
+    // 用固定的算法，打乱密匙簿，增加随机性，好像很复杂，实际上对并不会增加密文的强度
+    for($j = $i = 0; $i < 256; $i++) {
+        $j = ($j + $box[$i] + $rndkey[$i]) % 256;
+        $tmp = $box[$i];
+        $box[$i] = $box[$j];
+        $box[$j] = $tmp;
+    }
+    // 核心加解密部分
+    for($a = $j = $i = 0; $i < $string_length; $i++) {
+        $a = ($a + 1) % 256;
+        $j = ($j + $box[$a]) % 256;
+        $tmp = $box[$a];
+        $box[$a] = $box[$j];
+        $box[$j] = $tmp;
+        // 从密匙簿得出密匙进行异或，再转成字符
+        $result .= chr(ord($string[$i]) ^ ($box[($box[$a] + $box[$j]) % 256]));
+    }
+    if($operation == 'D') {
+        // 验证数据有效性，请看未加密明文的格式
+        if((substr($result, 0, 10) == 0 || substr($result, 0, 10) - time() > 0) &&
+substr($result, 10, 16) == substr(md5(substr($result, 26).$keyb), 0, 16)) {
+            return substr($result, 26);
+        } else {
+            return '';
+        }
+    } else {
+        // 把动态密匙保存在密文里，这也是为什么同样的明文，生产不同密文后能解密的原因
+        // 因为加密后的密文可能是一些特殊字符，复制过程可能会丢失，所以用base64编码
+        return $keyc.str_replace('=', '', base64_encode($result));
+    }
 }
 ?>
